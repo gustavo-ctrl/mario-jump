60dc3056b88784f22f29dfcb0a14529cdc687b7b
Gustavo Silva <gustavo.silvasouzade@gmail.comm>
2026-05-21 20:42:31 -0300

    corrigindo layout do jogo

--- script.js
+++ script.js
@@ -1,62 +1,37 @@
-const mario = document.querySelector('.mario');
-const pipes = document.querySelectorAll('.pipe');
-const scoreBoard = document.querySelector('.score');
-const gameOverScreen = document.querySelector('.game-over');
-const restartBtn = document.querySelector('#restartBtn');
+const player = document.getElementById("player");
 
-let score = 0;
-let gameOver = false;
+let isJumping = false;
+let position = 100;
+let gravity = 5;
+let jumpLimit = 180;
 
-/* PULO */
-const jump = () => {
-    if (mario.classList.contains('jump') || gameOver) return;
-
-    mario.classList.add('jump');
-
-    setTimeout(() => {
-        mario.classList.remove('jump');
-    }, 850);
-};
-
-document.addEventListener('keydown', jump);
-
-/* LOOP DO JOGO */
-let loop = setInterval(() => {
-
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
-
-    pipes.forEach((pipe) => {
-
-        const pipePosition = pipe.offsetLeft;
-
-        /* COLISÃO */
-        if (pipePosition < 120 && pipePosition > 0 && marioPosition < 80) {
-
-            pipe.style.animation = 'none';
-            pipe.style.left = `${pipePosition}px`;
-
-            mario.style.animation = 'none';
-            mario.style.bottom = `${marioPosition}px`;
-
-            mario.src = "./imagens/game-over.png";
-
-            gameOver = true;
-            clearInterval(loop);
-
-            gameOverScreen.style.display = 'flex';
-        }
-
-        /* PONTUAÇÃO */
-        if (pipePosition < 0 && !gameOver) {
-            score++;
-            scoreBoard.innerHTML = score;
-        }
-
-    });
-
-}, 10);
-
-/* RESTART */
-restartBtn.addEventListener('click', () => {
-    location.reload();
+document.addEventListener("keydown", (e) => {
+  if (e.code === "Space") {
+    jump();
+  }
 });
+
+function jump() {
+  if (isJumping) return;
+
+  isJumping = true;
+
+  let upInterval = setInterval(() => {
+    if (position >= jumpLimit) {
+      clearInterval(upInterval);
+
+      let downInterval = setInterval(() => {
+        if (position <= 100) {
+          clearInterval(downInterval);
+          isJumping = false;
+        }
+
+        position -= gravity;
+        player.style.bottom = position + "px";
+      }, 20);
+    }
+
+    position += 6;
+    player.style.bottom = position + "px";
+  }, 20);
+}
